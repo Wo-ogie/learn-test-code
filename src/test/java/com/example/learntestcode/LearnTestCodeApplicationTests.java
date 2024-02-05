5d7ac53a277bfa5c67b7a33c6e2383475259537e
Wo-ogie <siwall0105@gmail.com>
2024-02-06 04:41:33 +0900

    프로젝트 생성 및 초기 설정

---
+++ src/test/java/com/example/learntestcode/LearnTestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learntestcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTestCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
